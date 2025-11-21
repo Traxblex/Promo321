ba9709079e07cf26e566068aa1dde7a57570cdb5
Allama <allamacamara1@gmail.com>
2025-11-21 11:25:23 +0100

    debut du projet

--- info/vendredi14nov/Bdd/scrypte.sql
+++ info/vendredi14nov/Bdd/scrypte.sql
@@ -38,4 +38,51 @@ CREATE TABLE voiture (
 );
 
 /*ajouter une ligne dans la table client*/
+/* insert into sert a ajouter une ligne dans une table*/
 INSERT INTO client (id, nom, prenom, email, mdp, date_naissance, adresse) VALUES ( 1, "Uchiwa", "Itachi", "itachi@gmail.com", "Louvre", "2000-08-23", "3 rue de konoha");
+
+INSERT INTO commande (id, numero_commande, montant_total, adresse_livraison, date_commande) VALUES (1, "CMD001", 55000.00, "3 rue de konoha", "2024-06-15");
+
+INSERT INTO client (nom, prenom, email, mdp, date_naissance, adresse) VALUES ("Uzumaki", "Naruto", "naruto@gmail.com", "Louvre", "2005-08-23", "31 rue de konoha");
+
+INSERT INTO COMMANDE (numero_commande, montant_total, adresse_livraison, date_commande) VALUES ("CMD002", 75000.00, "31 rue de konoha", "2024-06-16");
+INSERT INTO client (nom, prenom, email, mdp, date_naissance, adresse) VALUES ("Haruno", "Sakura", "sakura@gmail.com", "Louvre", "2003-08-23", "5 rue de konoha");
+INSERT INTO COMMANDE (numero_commande, montant_total, adresse_livraison, date_commande) VALUES ("CMD0003", 65000.00, "5 rue de konoha", "2024-06-17");
+insert INTO client (nom, prenom, email, mdp, date_naissance, adresse) VALUES ("Uchiwa", "Sasuke", "sasuke@gmail.com", "Louvre", "2004-08-23", "7 rue de konoha");
+insert into COMMANDE (numero_commande, montant_total, adresse_livraison, date_commande) VALUES ("CMD004", 65000.00, "7 rue de konoha", "2024-06-17");
+INSERT INTO client (nom, prenom, email, mdp, date_naissance, adresse) VALUES ("Nara", "Shikamaru", "shikamaru@gmail.com", "Louvre", "2002-08-23", "9 rue de konoha");
+INSERT INTO COMMANDE (numero_commande, montant_total, adresse_livraison, date_commande) VALUES ("CMD005", 80000.00, "7 rue de konoha", "2024-06-18");
+INSERT INTO client (nom, prenom, email, mdp, date_naissance, adresse) VALUES ("Akimichi", "Choji", "choji@gmail.com", "Louvre", "2001-08-23", "11 rue de konoha");
+INSERT INTO COMMANDE (numero_commande, montant_total, adresse_livraison, date_commande) VALUES ("CMD006", 70000.00, "11 rue de konoha", "2024-06-19");
+INSERT INTO client (nom, prenom, email, mdp, date_naissance, adresse) VALUES ("Yamanaka", "Ino", "ino@gmail.com", "Louvre", "2003-05-12", "13 rue de konoha");
+INSERT INTO COMMANDE (numero_commande, montant_total, adresse_livraison, date_commande) VALUES ("CMD007", 72000.00, "9 rue de konoha", "2024-06-19");
+INSERT INTO client (nom, prenom, email, mdp, date_naissance, adresse) VALUES ("Sarutobi", "Asuma", "asuma@gmail.com", "Louvre", "1995-09-07", "15 rue de konoha");
+INSERT INTO COMMANDE (numero_commande, montant_total, adresse_livraison, date_commande) VALUES ("CMD008", 68000.00, "11 rue de konoha", "2024-06-20");
+INSERT INTO client (nom, prenom, email, mdp, date_naissance, adresse) VALUES ("Tsunade", "Senju", "senju@gmail.com", "Louvre", "1980-03-02", "17 rue de konoha");
+INSERT INTO commande (numero_commande, montant_total, adresse_livraison, date_commande) VALUES ("CMD009", 90000.00, "13 rue de konoha", "2024-06-21");
+INSERT INTO client (nom, prenom, email, mdp, date_naissance, adresse) VALUES ("Hyuuga", "Hinata", "hinata@gmail.com", "Louvre", "2004-12-27", "19 rue de konoha");
+INSERT INTO COMMANDE (numero_commande, montant_total, adresse_livraison, date_commande) VALUES ("CMD010", 76000.00, "15 rue de konoha", "2024-06-22");
+
+/* RECUPERAR DEPUIS LA TABLE CLIENT*/
+/*select sert a recuperer des donnees depuis une table*/
+select * from client;
+/*recuperer selon un critere*/
+select * from client where email="itachi@gmail.com";
+select * from client where email="itachi@gmail.com" AND nom="Itachi";
+select * from client where email="itachi@gmail.com" or nom="Uchiwa";
+/*suppresion d'une ligne dans la table client*/
+delete from client where id = 1;
+/*modifier une ligne dans la table client */
+/* Set sert à affecter une nouvelle valeur à un champ, dans le champ update il n'y a pas besoins de from*/
+update client set nom="TOTO" where id=2;
+
+#1 : afficher tous les commandes
+select * from commande;
+#2 : afficher les commandes dont le montant est superieur a 70000
+select * from commande where montant_total > 70000;
+#3 : afficher les 5 commandes les plus chères et order by veut dire trier par et DESC veut dire du plus grand au plus petit
+select * from commande order by montant_total DESC limit 5;
+#4 : trier les commandes par date de libraison la plus proche et asc veut dire du plus petit au plus grand
+select * from commande order by date_commande ASC;
+#5 : compter toutes les commmandes et count(*) veur dire compter toutes les lignes et AS permet de renommer le resultat
+select count(*) AS nombre_commande from commande
---
+++ info/vendredi21nov/eleve.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BTS SLAM 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">PROMO 321</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="matiere/info.php">INFO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="matiere/math.php">MATH</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="matiere/anglais.php">ANGLAIS</a>
+                        </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="matiere/francais.php">FRANCAIS</a>
+                        </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="matiere/cejm.php">CEJM</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+ <main class="container mt-4">
+        <h1>Liste des élève</h1>
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nom</th>
+      <th scope="col">Prénom</th>
+      <th scope="col">Email</th>
+      <th scope="col">Discord</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>ADDAD</td>
+      <td>Abdelkarim</td>
+      <td>@</td>
+      <td>@karim_addad</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>CAMARA </td>
+      <td>Allama</td>
+      <td>@</td>
+      <td>@trax_black</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>CHERFI  </td>
+      <td>Moaad</td>
+      <td>@</td>
+      <td>@moaadc</td>
+    </tr>
+    <tr>
+        <th scope="row">4</th>
+        <td>DIALLO</td>
+        <td>Mamadou</td>
+        <td>@</td>
+        <td>@madwest</td>
+    </tr>
+    <tr>
+        <th scope="row">5</th>
+        <td>FLECHAS LAGUNA</td>
+        <td>Tania</td>
+        <td>@</td>
+        <td>@tania002198</td>
+    </tr>
+    <tr>
+        <th scope="row">6</th>
+        <td>FOUKOU SALET</td>
+        <td>Abdoulaye</td>
+        <td>@</td>
+        <td>@ab236_</td>
+    </tr>
+    <tr>
+        <th scope="row">7</th>
+        <td>GOTAL HENRY</td>
+        <td>Stanley</td>
+        <td>@</td>
+        <td>@stanley02840</td>
+    </tr>
+    <tr>
+        <th scope="row">8</th>
+        <td>KABA</td>
+        <td>Ismael</td>
+        <td>@</td>
+        <td>@</td>
+    </tr>
+    <tr>
+        <th scope="row">9</th>
+        <td>KENNICHE</td>
+        <td>Miryam</td>
+        <td>@</td>
+        <td>@miryam.m</td>
+    </tr>
+    <tr>
+        <th scope="row">10</th>
+        <td>MANAC'H</td>
+        <td>Evan</td>
+        <td>@</td>
+        <td>@</td>
+    </tr>
+    <tr>
+        <th scope="row">11</th>
+        <td>PICORELLI HAMORI</td>
+        <td>Elysa</td>
+        <td>@</td>
+        <td>@elysa.exe</td>
+    </tr>
+    <tr>
+        <th scope="row">12</th>
+        <td>PRUDENT </td>
+        <td>Matthieu</td>
+        <td>@</td>
+        <td>@matt_pg</td>
+    </tr>
+    <tr>
+        <th scope="row">13</th>
+        <td>SAHIE </td>
+        <td>Jeriel</td>
+        <td>@</td>
+        <td>neyzoks_89</td>
+    </tr>
+    <tr>
+        <th scope="row">14</th>
+        <td>TALL </td>
+        <td>Rayan</td>
+        <td>@</td>
+        <td>@</td>
+    </tr>
+    <tr>
+        <th scope="row">15</th>
+        <td>ZENKRI</td>
+        <td>Ahmed</td>
+        <td>@</td>
+        <td>@amd93</td>
+    </tr>
+    <tr>
+        <th scope="row">16</th>
+        <td>SHETA</td>
+        <td>Abdalla</td>
+        <td>@</td>
+        <td>@</td>
+    </tr>
+
+  </tbody>
+</table>
+    </main>
+    <footer class="mt-auto">
+        <div class="bg-light text-center py-3 mt-4 fixed-bottom">
+            &copy; 2025 BTS SLAM 1 - Promo 321
+        </div>
+    </footer>
+</body>
+</html>
